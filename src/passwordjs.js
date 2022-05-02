f17f88a9c0b922d8a27bf15010268eae162b5a13
Dylan <99437817+gainesd2@users.noreply.github.com>
2022-05-01 21:08:09 -0400

    Passwordjs now functional
    > passwordjs.js now functional and documented, and passes the acceptance test
    | -- the jest test file is blank because the passwordjs() returns its value as a string.

--- src/passwordjs.js
+++ src/passwordjs.js
@@ -3,26 +3,58 @@ const fs = require('fs');
 const util = require('./utility')
 
 /**
- * Main function, calls sub to process
+ * Main function, calls verifyUser() to process
  */
 function passwordjs() {
     if (process.argv.length != 5) return 'false';
 
-    var filename = process.argv[2]
-    var email = process.argv[3]
-    var password = process.argv[4]
+    let filename = process.argv[2];
+    let email = process.argv[3];
+    let password = process.argv[4];
 
-    // ???
+    return verifyUser(filename, email, password).toString();
 }
 
 /**
  * Verifies that an enail or password exists in a hashed file
- * @param fileName file containing email and hashed passwords
- * @param email email of user
- * @param password unhashed input password
+ * @param fileName {string} file containing email and hashed passwords
+ * @param email {string} email of user
+ * @param password {string} unhashed input password
+ * @return {boolean}
  */
 function verifyUser(fileName, email, password) {
+    // blank email
+    if (email == "") {
+        return false
+    }
+    // blank password
+    else if (password == "") {
+        return false
+    }
+    // simple regex
+    else if (!(/^[A-Za-z0-9+_.-]+@(.+)$/.test(email))) {
+        return false
+    } else {
+        let hashedPassword = util.hash(password);
 
+        try {
+            let fileData = util.readFile(fileName);
+
+            let entry = fileData.filter(val => {
+                return val.includes(email);
+            });
+
+            if (entry.length < 1) {
+                return false;
+            }
+
+            let parts = entry[0].split(":");
+            return parts[1] == hashedPassword;
+        } catch (err) {
+            return false;
+        }
+
+    }
 }
 
 if (require.main === module) {
--- tests/passwordjs.test.js
+++ tests/passwordjs.test.js
@@ -1,3 +1,3 @@
 // Make tests when you have sub functions in this module.
 // passwordjs() is tested by acceptance tests (acceptance.bat)
-
+// verifyUser() is thereore tested as well
